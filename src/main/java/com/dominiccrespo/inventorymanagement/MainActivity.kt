9416aff2cea1ac7c06f19e35437f5031cdb4a9a9
Dominic Crespo <dominic_crespo@yahoo.com>
2020-12-25 23:10:27 -0800

    First commit

---
+++ src/main/java/com/dominiccrespo/inventorymanagement/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dominiccrespo.inventorymanagement
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        database = Firebase.database.reference
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
---
+++ src/main/java/com/dominiccrespo/inventorymanagement/ViewAllProductsFragment.kt
@@ -0,0 +1,27 @@
+package com.dominiccrespo.inventorymanagement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class ViewAllProductsFragment : Fragment() {
+    private lateinit var database: DatabaseReference
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_all_products, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        database = Firebase.database.reference
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
